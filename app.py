8fc107f8eb2aaf155c4f5a5f516da845e17a0b29
malo21st <44739330+malo21st@users.noreply.github.com>
2023-10-13 19:58:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -78,8 +78,5 @@ if st.button("トークンをカウント", type="primary"):
     result_1.metric(label="カウント結果：", value=f"{tokens_1} tokens")
     result_2.metric(label="カウント結果：", value=f"{tokens_2} tokens")
 
-st.markdown('''   
-
-   
-:balloon:[openai-cookbook/examples/How_to_count_tokens_with_tiktoken.ipynb](https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb)
-''')
+st.write("\n\n")
+st.markdown(':balloon:[openai-cookbook/examples/How_to_count_tokens_with_tiktoken.ipynb](https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb)')
